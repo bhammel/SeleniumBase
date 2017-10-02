489e819ccb59999f34ee1f9e9229093f408de3aa
bhammel <bhammel@umail.ucsb.edu>
2017-10-01 19:04:11 -0700

    Initial commit

---
+++ SeleniumBase/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtensions;
+
+namespace SeleniumBase
+{
+    public class Program
+    {
+        // Constants
+        private const string mc_strUrl = "https://www.google.com/"; // Replace with the URL you want to navigate to
+        private const double mc_dblTimeout = 1; // The web driver timeout in minutes
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                // Initialize the web driver
+                using (IWebDriver objDriver = new ChromeDriver())
+                {
+                    // Initialize the web driver wait
+                    WebDriverWait objWait = new WebDriverWait(objDriver, TimeSpan.FromMinutes(mc_dblTimeout));
+                    objWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+
+                    // Navigate to the specified URL
+                    objDriver.Navigate().GoToUrl(mc_strUrl);
+
+                    /*
+                     * The rest of your code goes here.
+                     * 
+                     * Here is an example of a common pattern to use:
+                     */
+
+                    // Wait until the element with id "lst-ib" exists on the page
+                    objWait.Until(d => d.FindElementById("lst-ib"));
+
+                    // Get the element with id "lst-ib"
+                    IWebElement objSearchBox = objDriver.FindElementById("lst-ib");
+
+                    // Send the element some text input
+                    objSearchBox.SendKeys("kittens");
+                    objSearchBox.SendKeys(Keys.Enter);
+
+                    // Wait indefinitely. This is just so the browser doesn't automatically close.
+                    objWait.Until(d => false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine(ex.Message);
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ SeleniumExtensions/SeleniumExtensions.cs
@@ -0,0 +1,103 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumExtensions
+{
+    public static class SeleniumExtensions
+    {
+        public static IWebElement FindElementByClassName(this ISearchContext objContext, string strClassNameToFind)
+        {
+            try
+            {
+                return objContext.FindElement(By.ClassName(strClassNameToFind));
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+        }
+
+        public static IWebElement FindElementByCssSelector(this ISearchContext objContext, string strCssSelectorToFind)
+        {
+            try
+            {
+                return objContext.FindElement(By.CssSelector(strCssSelectorToFind));
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+        }
+
+        public static IWebElement FindElementById(this ISearchContext objContext, string strIdToFind)
+        {
+            try
+            {
+                return objContext.FindElement(By.Id(strIdToFind));
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+        }
+
+        public static IWebElement FindElementByLinkText(this ISearchContext objContext, string strLinkTextToFind)
+        {
+            try
+            {
+                return objContext.FindElement(By.LinkText(strLinkTextToFind));
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+        }
+
+        public static IWebElement FindElementByName(this ISearchContext objContext, string strNameToFind)
+        {
+            try
+            {
+                return objContext.FindElement(By.Name(strNameToFind));
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+        }
+
+        public static IWebElement FindElementByPartialLinkText(this ISearchContext objContext, string strPartialLinkTextToFind)
+        {
+            try
+            {
+                return objContext.FindElement(By.PartialLinkText(strPartialLinkTextToFind));
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+        }
+
+        public static IWebElement FindElementByTagName(this ISearchContext objContext, string strTagNameToFind)
+        {
+            try
+            {
+                return objContext.FindElement(By.TagName(strTagNameToFind));
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+        }
+
+        public static IWebElement FindElementByXPath(this ISearchContext objContext, string strXPathToFind)
+        {
+            try
+            {
+                return objContext.FindElement(By.XPath(strXPathToFind));
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+        }
+    }
+}
